f9bcf0cd9383dc43da23249dededc3f6c334f8bb
Kevin Guertin <kevin.m.guertin@gmail.com>
2020-04-01 23:13:08 +0000

    add basic admin auth feature

--- Gemfile
+++ Gemfile
@@ -35,7 +35,7 @@ gem 'carrierwave'
 gem 'rmagick'
 gem 'stripe'
 gem 'faker'
-
+gem 'dotenv'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,8 +9,8 @@ class OrdersController < ApplicationController
     order  = create_order(charge)
 
     if order.valid?
-      redirect_to order, notice: 'Your Order has been placed.'
       empty_cart!
+      redirect_to order, notice: 'Your Order has been placed.'
     else
       redirect_to cart_path, flash: { error: order.errors.full_messages.first }
     end
