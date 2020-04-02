bf5f4ede9994de9c28ad2f08863d38a0337963fa
Kevin Guertin <kevin.m.guertin@gmail.com>
2020-04-02 15:51:06 +0000

    add about page and controller

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  resources :about, only: [:show]
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
