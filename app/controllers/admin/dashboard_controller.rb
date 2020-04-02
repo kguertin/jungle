62f3d5de5d8d327e4ebde8d6117257b1590b0a00
Kevin Guertin <kevin.m.guertin@gmail.com>
2020-04-02 00:50:44 +0000

    add number of products and number of categories in admindashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
 
   def show
+    @number_of_products = Product.all.count
+    @number_of_categories = Category.all.length
   end
 end
