de042996bf278978cd7e75ddb7d9562bc6f8123e
Kevin Guertin <kevin.m.guertin@gmail.com>
2020-04-02 23:27:13 +0000

    add user login and log out with user email

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,13 @@ Rails.application.routes.draw do
 
   resources :about, only: [:show]
 
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
